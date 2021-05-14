d64ea4f1dcea365f55616ba5d63d5a99c1c256a3
Bluesaxorcist <joshua@operatorfoundation.org>
2021-05-13 23:51:13 -0500

    implemented all functions of cloudloopAPI

--- Sources/CloudloopTemplate/CloudloopAPI.swift
+++ Sources/CloudloopTemplate/CloudloopAPI.swift
@@ -189,6 +189,41 @@ public struct CloudloopAPI
                 "currency": .string,
                 "id": .identifier,
                 "minimum": .float
+            ]),
+            ResultType(name: "Messages", fields: [
+                "id": .identifier,
+                "hardware": .identifier,
+                "payload": .string,
+                "txAt": .date,
+                "rxAt": .date,
+                "cdrReference": .float,
+                "cepRadius": .float,
+                "latitude": .float,
+                "longitude": .float,
+                "momsn": .float,
+                "status": .string
+            ]),
+            ResultType(name: "Requests", fields: [
+                "requestAt": .optional(.date),
+                "message": .optional(.identifier),
+                "hardware": .identifier,
+                "status": .string
+            ]),
+            ResultType(name: "DataMTMessages", fields: [
+                "createdAt": .date,
+                "queueSize": .float,
+                "size": .float,
+                "payload": .string,
+                "id": .identifier,
+                "submittedAt": .date,
+                "hardware": .identifier,
+                "status": .string
+            ]),
+            ResultType(name: "BillingGroups", fields: [
+                "default": .boolean,
+                "name": .string,
+                "id": .identifier,
+                "account": .identifier
             ])
         ],
         endpoints: [
@@ -970,6 +1005,351 @@ public struct CloudloopAPI
                     )
                 ]
             ),
+            Endpoint(
+                name: "Data (MO)",
+                documentation: URL(string: "https://docs.cloudloop.com/reference#retrieve-messages")!,
+                functions: [
+                    Function(
+                        name: "ReceiveMessage",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#retrieve-messages")!,
+                        resultType: ResultType(name: "ReceiveMessageResult",
+                            fields: [
+                                "messages": .array(.structure("Messages"))
+                            ]
+                        ),
+                        parameters: [
+                            Parameter(
+                                name: "hardware",
+                                description: "CloudLoop Hardware ID",
+                                type: .identifier
+                            ),
+                            Parameter(
+                                name: "from",
+                                description: "Using the ISO_LOCAL_DATE_TIME format",
+                                type: .date
+                            ),
+                            Parameter(
+                                name: "hardware",
+                                description: "Using the ISO_LOCAL_DATE_TIME format",
+                                type: .date
+                            )
+                        ]
+                    ),
+                    Function(
+                        name: "ReceiveMessage (Long Poll)",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#retrieve-messages-long-poll")!,
+                        resultType: ResultType(name: "ReceiveMessageLongPollResult",
+                            fields: [
+                                "lastMessageRetreived": .identifier,
+                                "messages": .array(.structure("Messages"))
+                            ]
+                        ),
+                        parameters: [
+                            Parameter(
+                                name: "maxPollTime",
+                                description: "Maximum Poll Time (0 - 60 secs)",
+                                type: .int32
+                            ),
+                            Parameter(
+                                name: "lastMessageReceived",
+                                description: "Last Message Id retrieved by client (omitted, if unknown)",
+                                type: .string
+                            )
+                        ]
+                    )
+                ]
+            ),
+            Endpoint(
+                name: "Data (MT)",
+                documentation: URL(string: "https://docs.cloudloop.com/reference#send-message")!,
+                functions: [
+                    Function(
+                        name: "SendMessage",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#send-message")!,
+                        resultType: ResultType(name: "SendMessageResult",
+                            fields: [
+                                "requests": .array(.structure("Requests"))
+                            ]
+                        ),
+                        parameters: [
+                            Parameter(
+                                name: "hardware",
+                                description: "csv list of cloudloop hardware id",
+                                type: .identifier
+                            ),
+                            Parameter(
+                                name: "payload",
+                                description: "hex-encoded message payload",
+                                type: .string
+                            )
+                        ]
+                    ),
+                    Function(
+                        name: "GetMessage",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#get-message-status")!,
+                        resultType: ResultType(name: "GetMessageResult",
+                            fields: [
+                                "id": .identifier,
+                                "status": .string,
+                                "createdAt": .date,
+                                "payload": .string,
+                                "submittedAt": .date,
+                                "hardware": .identifier,
+                                "queueSize": .float
+                            ]
+                        ),
+                        parameters: [
+                            Parameter(
+                                name: "message",
+                                description: "cloudloop mt message id",
+                                type: .string
+                            )
+                        ]
+                    ),
+                    Function(
+                        name: "GetMessages",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#get-messages")!,
+                        resultType: ResultType(name: "GetMessagesResult",
+                            fields: [
+                                "messages": .array(.structure("DataMTMessages"))
+                            ]
+                        ),
+                        parameters: [
+                            Parameter(
+                                name: "hardware",
+                                description: "cloudloop hardware id",
+                                type: .identifier
+                            ),
+                            Parameter(
+                                name: "from",
+                                description: "Using the ISO_LOCAL_DATE_TIME format",
+                                type: .date
+                            ),
+                            Parameter(
+                                name: "to",
+                                description: "Using the ISO_LOCAL_DATE_TIME format",
+                                type: .date
+                            )
+                        ]
+                    ),
+                    Function(
+                        name: "GetMessagesPolled",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#getmessagespolled")!,
+                        resultType: ResultType(name: "GetMessagesPolledResult",
+                            fields: [
+                                "messages": .array(.structure("DataMTMessages"))
+                            ]
+                        ),
+                        parameters: [
+                            Parameter(
+                                name: "lastMessageRetrieved",
+                                description: "ID of last message seen",
+                                type: .identifier
+                            ),
+                            Parameter(
+                                name: "maxPollTime",
+                                description: "Maximum Poll Time (0 - 60 secs)",
+                                type: .int32
+                            )
+                        ]
+                    ),
+                ]
+            ),
+            Endpoint(
+                name: "Hardware",
+                documentation: URL(string: "https://docs.cloudloop.com/reference#create-hardware")!,
+                functions: [
+                    Function(
+                        name: "CreateHardware",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#create-hardware")!,
+                        resultType: ResultType(name: "CreateHardwareResult",
+                            fields: [
+                                "imei": .string,
+                                "id": .identifier,
+                                "type": .string
+                            ]
+                        ),
+                        parameters: [
+                            Parameter(
+                                name: "imei",
+                                type: .string
+                            ),
+                            Parameter(
+                                name: "type",
+                                description: "Type (IRIDIUM_SBD, IRIDIUM_TELEPHONY)",
+                                type: .string
+                            )
+                        ]
+                    ),
+                    Function(
+                        name: "GetHardware",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#get-hardware")!,
+                        resultType: ResultType(name: "GetHardwareResult",
+                            fields: [
+                                "imei": .string,
+                                "id": .identifier,
+                                "type": .string
+                            ]
+                        ),
+                        parameters: [
+                            Parameter(
+                                name: "hardware",
+                                description: "Cloudloop Hardware Id",
+                                type: .identifier
+                            ),
+                            Parameter(
+                                name: "imei",
+                                type: .string
+                            )
+                        ]
+                    ),
+                    Function(
+                        name: "SearchHardware",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#search-hardware")!,
+                        resultType: ResultType(name: "SearchHardwareResult",
+                            fields: [
+                                "imei": .string,
+                                "id": .identifier,
+                                "type": .string
+                            ]
+                        ),
+                        parameters: []
+                    )
+                ]
+            ),
+            Endpoint(
+                name: "Sim",
+                documentation: URL(string: "https://docs.cloudloop.com/reference#create-sim")!,
+                functions: [
+                    Function(
+                        name: "CreateSim",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#create-sim")!,
+                        resultType: ResultType(name: "CreateSimResult",
+                            fields: [
+                                "iccid": .string,
+                                "id": .identifier
+                            ]
+                        ),
+                        parameters: [
+                            Parameter(
+                                name: "iccid",
+                                type: .string
+                            )
+                        ]
+                    ),
+                    Function(
+                        name: "GetSim",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#get-sim")!,
+                        resultType: ResultType(name: "GetSimResult",
+                            fields: [
+                                "sim": .string,
+                                "id": .identifier
+                            ]
+                        ),
+                        parameters: [
+                            Parameter(
+                                name: "sim",
+                                description: "CloudLoop Sim ID",
+                                type: .string
+                            )
+                        ]
+                    ),
+                    Function(
+                        name: "SearchSims",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#get-sims")!,
+                        resultType: ResultType(name: "SearchSimsResult",
+                            fields: [
+                                "iccid": .string,
+                                "id": .identifier
+                            ]
+                        ),
+                        parameters: []
+                    )
+                ]
+            ),
+            Endpoint(
+                name: "Account",
+                documentation: URL(string: "https://docs.cloudloop.com/reference#create-billing-group")!,
+                functions: [
+                    Function(
+                        name: "CreateBillingGroup",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#create-billing-group")!,
+                        resultType: ResultType(name: "CreateBillingGroup",
+                            fields: [
+                                "default": .boolean,
+                                "name": .string,
+                                "id": .identifier,
+                                "account": .identifier
+                            ]
+                        ),
+                        parameters: [
+                            Parameter(
+                                name: "name",
+                                description: "Name of the billing group",
+                                type: .string
+                            )
+                        ]
+                    ),
+                    Function(
+                        name: "GetBillingGroups",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#get-billing-groups")!,
+                        resultType: ResultType(name: "GetBillingGroupsResult",
+                            fields: [
+                                "billingGroup": .array(.structure("BillingGroups"))
+                            ]
+                        ),
+                        parameters: []
+                    ),
+                    Function(
+                        name: "DeleteBillingGroup",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#delete-billing-group")!,
+                        resultType: ResultType(name: "DeleteBillingGroupResult",
+                            fields: [
+                                "result": .boolean
+                            ]
+                        ),
+                        parameters: [
+                            Parameter(
+                                name: "BillingGroup",
+                                description: "ID of the billing group(cannot be the default)",
+                                type: .string
+                            )
+                        ]
+                    )
+                ]
+            ),
+            Endpoint(
+                name: "User",
+                documentation: URL(string: "https://docs.cloudloop.com/reference#generate-token")!,
+                functions: [
+                    Function(
+                        name: "GenerateToken",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#generate-token")!,
+                        resultType: ResultType(name: "GenerateTokenResult",
+                            fields: [
+                                "token": .identifier
+                            ]
+                        ),
+                        parameters: []
+                    )
+                ]
+            ),
+            Endpoint(
+                name: "Platform",
+                documentation: URL(string: "https://docs.cloudloop.com/reference#ping")!,
+                functions: [
+                    Function(
+                        name: "Ping",
+                        documentation: URL(string: "https://docs.cloudloop.com/reference#ping")!,
+                        resultType: ResultType(name: "PingResult",
+                            fields: [
+                                "ping": .string
+                            ]
+                        ),
+                        parameters: []
+                    )
+                ]
+            )
         ]
     )
 
