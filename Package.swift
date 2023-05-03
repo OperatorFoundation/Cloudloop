0ce1b7ecb68218be3bc0d355824dc6d7988ffda0
consuelita <adelita@operatorfoundation.org>
2023-05-03 18:48:48 -0500

    Supports .iOS(.v14)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Cloudloop",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v12), .iOS(.v14)],
     products: [
         .library(
             name: "Cloudloop",
