97e23e79cb900647dc7abeb6683097d139d914a2
bragelbytes <joebragel@gmail.com>
2023-06-27 14:17:39 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Cloudloop",
-    platforms: [.macOS(.v12), .iOS(.v15)],
+    platforms: [.macOS(.v13), .iOS(.v15)],
     products: [
         .library(
             name: "Cloudloop",
